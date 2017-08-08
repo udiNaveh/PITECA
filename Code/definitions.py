ca6b1fa3f037458f5cb8940cac71c0c701f8cbd2
udiNaveh <udi.naveh@gmail.com>
2017-08-09 00:26:12 +0300

    ADD MORE DATA AND DEFINITIONS

---
+++ Code/definitions.py
@@ -0,0 +1,16 @@
+import os
+
+CODE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.split(CODE_DIR)[0]
+CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.ini')
+
+DATA_DIR = os.path.join(ROOT_DIR, 'Data')
+PREDICTIONS_DIR = os.path.join(DATA_DIR, 'Predictions')
+MODELS_DIR = os.path.join(DATA_DIR, 'Models')
+LINEAR_MODEL_DIR = os.path.join(MODELS_DIR, 'LinearModel')
+EXTRACTED_FEATURES_DIR = os.path.join(DATA_DIR, 'ExtractedFeatures')
+ANALYSIS_DIR = os.path.join(DATA_DIR, 'Analysis')
+LINEAR_MODEL_BETAS_PATH = os.path.join(LINEAR_MODEL_DIR, 'average_betas_100_subjects_7_tasks.npy')
+SC_CLUSTERS_PATH = os.path.join(MODELS_DIR, 'FeatureExtractor', 'SC_clusters.dtseries.nii')
+ICA_LR_MATCHED_PATH = os.path.join(MODELS_DIR, 'FeatureExtractor', 'ica_LR_MATCHED.dtseries.nii')
+ICA_LOW_DIM_PATH= os.path.join(MODELS_DIR,'ica_both_lowdim.dtseries.nii')
